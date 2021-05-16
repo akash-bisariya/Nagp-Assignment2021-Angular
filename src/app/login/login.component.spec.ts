4ab2d470986e2b847a717e6ac0a47f05009d7e6d
akash-bisariya <bigakash@gmail.com>
2021-05-16 22:23:45 +0530

    test case added for login form

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,6 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 fdescribe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,6 +11,8 @@ fdescribe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports:[FormsModule, BrowserAnimationsModule.forRoot(),
+        ReactiveFormsModule,TranslateModule.forRoot()],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
@@ -26,4 +31,10 @@ fdescribe('LoginComponent', () => {
     expect(component).toBeTruthy();
     component.submitLogin();
   });
+
+  it(`should should Login text'`, async(() => {
+    fixture = TestBed.createComponent(LoginComponent);
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Login');
+  }));
 });
